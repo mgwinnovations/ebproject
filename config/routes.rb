05cd1068e59faeae6e3b07c323772a646a8cf901
wilsonmicah <micah@mgwinnovations.com>
2017-07-25 16:12:14 -0500

    updated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome_page/index'
 
-  root 'welcome_page#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
